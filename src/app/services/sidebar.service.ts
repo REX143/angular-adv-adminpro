840d00fc0853f4bf40f5343d3aeeeeff83edea8e
Herberts Aldava Asencios <h.gaia@live.com>
2022-02-22 13:12:24 -0500

    Fin sección 7

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  menu: any[] = [
+    { titulo: 'PRINCIPAL',
+      icono: 'mdi mdi-gauge',
+      submenu: [
+        {titulo: 'Main', url: '/'},
+        {titulo: 'Barra de progreso', url: 'progress'},
+        {titulo: 'Graficas', url: 'grafica1'},
+      ]
+    }
+  ]
+  constructor() { }
+}
